8b224efd7ed288c8f2f5468c50a4682ca2c4be8b
Rafsan Ahmad <rafsanahmad111@gmail.com>
2021-10-08 01:11:13 +0600

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/cupertino.dart';
 
+import 'package:provider/provider.dart';
+
 import 'app.dart';
+import 'model/app_state_model.dart';
 
 void main() {
-  return runApp(const CupertinoStoreApp());
+  return runApp(
+    ChangeNotifierProvider<AppStateModel>(
+      create: (_) => AppStateModel()..loadProducts(),
+      child: const CupertinoStoreApp(),
+    ),
+  );
 }
--- lib/model/app_state_model.dart
+++ lib/model/app_state_model.dart
@@ -1,1 +1,39 @@
-// TODO Implement this library.
+import 'package:flutter/foundation.dart' as foundation;
+
+import 'product.dart';
+import 'products_repository.dart';
+
+double _salesTaxRate = 0.06;
+double _shippingCostPerItem = 7;
+
+class AppStateModel extends foundation.ChangeNotifier {
+  // All the available products.
+  List<Product> _availableProducts = [];
+
+  // The currently selected category of products.
+  Category _selectedCategory = Category.all;
+
+  // The IDs and quantities of products currently in the cart.
+  final _productsInCart = <int, int>{};
+
+  Map<int, int> get productsInCart {
+    return Map.from(_productsInCart);
+  }
+
+  // Total number of items in the cart.
+  int get totalCartQuantity {
+    return _productsInCart.values.fold(0, (accumulator, value) {
+      return accumulator + value;
+    });
+  }
+
+  Category get selectedCategory {
+    return _selectedCategory;
+  }
+
+  // Loads the list of available products from the repo.
+  void loadProducts() {
+    _availableProducts = ProductsRepository.loadProducts(Category.all);
+    notifyListeners();
+  }
+}
