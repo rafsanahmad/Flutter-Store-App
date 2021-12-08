e06f8f0e7ea3dfc1145aaff94f9360cb75292ba1
Rafsan Ahmad <rafsanahmad111@gmail.com>
2021-12-09 01:10:32 +0600

    product list tab, return assymetric view

--- lib/pages/product_list_tab.dart
+++ lib/pages/product_list_tab.dart
@@ -1,45 +1,16 @@
 import 'package:flutter/cupertino.dart';
-import 'package:provider/provider.dart';
-
-import '../model/app_state_model.dart';
-import '../ui/product_row_item.dart';
+import 'package:mina_store/model/product.dart';
+import 'package:mina_store/model/products_repository.dart';
+import 'package:mina_store/ui/assymetric_view.dart';
 
 class ProductListTab extends StatelessWidget {
-  const ProductListTab({Key? key}) : super(key: key);
+  final Category category;
+
+  const ProductListTab({this.category = Category.all, Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<AppStateModel>(
-      builder: (context, model, child) {
-        final products = model.getProducts();
-        return CustomScrollView(
-          semanticChildCount: products.length,
-          slivers: <Widget>[
-            const CupertinoSliverNavigationBar(
-              largeTitle: Text('Mina\'s Store'),
-            ),
-            SliverSafeArea(
-              // BEGINNING OF NEW CONTENT
-              top: false,
-              minimum: const EdgeInsets.only(top: 8),
-              sliver: SliverList(
-                delegate: SliverChildBuilderDelegate(
-                  (context, index) {
-                    if (index < products.length) {
-                      return ProductRowItem(
-                        product: products[index],
-                        lastItem: index == products.length - 1,
-                      );
-                    }
-
-                    return null;
-                  },
-                ),
-              ),
-            ) // END OF NEW CONTENT
-          ],
-        );
-      },
-    );
+    return AsymmetricView(products: ProductsRepository.loadProducts(category));
   }
 }
